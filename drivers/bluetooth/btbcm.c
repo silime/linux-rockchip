47ce9781c82a899047b755722bb2834644ae9703
silime <60662669+silime@users.noreply.github.com>
2025-02-20 08:25:08 +0000

    driver: bluetooth: add BCM4362A2 support

--- drivers/bluetooth/btbcm.c
+++ drivers/bluetooth/btbcm.c
@@ -449,6 +449,7 @@ static const struct bcm_subver_table bcm_uart_subver_table[] = {
 	{ 0x4217, "BCM4329B1"   },	/* 002.002.023 */
 	{ 0x6106, "BCM4359C0"	},	/* 003.001.006 */
 	{ 0x4106, "BCM4335A0"	},	/* 002.001.006 */
+	{ 0x1111, "BCM4362A2"	},	/* 000.017.017 */
 	{ }
 };
 
